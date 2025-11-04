085f17ffd8f2cdd9308958506e4871364c00c468
John Romkey <58883+romkey@users.noreply.github.com>
2025-11-04 13:58:43 -0800

    fix: correct sidekiq.yml path in initializer
    The initializer was looking for sidekiq.yml at /app/sidekiq.yml
    but the file is at /app/config/sidekiq.yml.
    
    Changed from ../../sidekiq.yml to ../sidekiq.yml to correctly
    navigate from config/initializers/ to config/sidekiq.yml

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,9 +5,9 @@ require 'sidekiq-scheduler'
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://redis:6379/0') }
 
-  # Load the schedule from sidekiq.yml
+  # Load the schedule from config/sidekiq.yml
   config.on(:startup) do
-    Sidekiq.schedule = YAML.load_file(File.expand_path('../../sidekiq.yml', __dir__))[:schedule]
+    Sidekiq.schedule = YAML.load_file(File.expand_path('../sidekiq.yml', __dir__))[:schedule]
     SidekiqScheduler::Scheduler.instance.reload_schedule!
   end
 end
