7de581865eb65fb8425a9f52e658d174dcf35454
John Romkey <58883+romkey@users.noreply.github.com>
2025-11-05 09:29:33 -0800

    Update schema.rb with can_create_events column
    - Updated schema version to 2025_11_05_170000
    - Added can_create_events boolean column to users table
    - Fixes pending migration error in CI tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_04_160453) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_05_170000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -146,6 +146,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_04_160453) do
     t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "can_create_events", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role"], name: "index_users_on_role"
