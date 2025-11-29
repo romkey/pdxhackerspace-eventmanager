d4debb064a757eecf22456c2ea2399105bbf9bc4
John Romkey <58883+romkey@users.noreply.github.com>
2025-11-28 17:22:58 -0800

    Update schema.rb with slug columns for CI tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_26_071509) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_28_235258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,10 +80,12 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_26_071509) do
     t.bigint "location_id"
     t.text "ai_reminder_7d"
     t.text "ai_reminder_1d"
+    t.string "slug"
     t.index ["event_id", "occurs_at"], name: "index_event_occurrences_on_event_id_and_occurs_at"
     t.index ["event_id"], name: "index_event_occurrences_on_event_id"
     t.index ["location_id"], name: "index_event_occurrences_on_location_id"
     t.index ["occurs_at"], name: "index_event_occurrences_on_occurs_at"
+    t.index ["slug"], name: "index_event_occurrences_on_slug", unique: true
     t.index ["status"], name: "index_event_occurrences_on_status"
   end
 
@@ -110,10 +112,12 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_26_071509) do
     t.boolean "draft", default: false, null: false
     t.boolean "slack_announce", default: true, null: false
     t.boolean "social_reminders", default: true, null: false
+    t.string "slug"
     t.index ["ical_token"], name: "index_events_on_ical_token", unique: true
     t.index ["location_id"], name: "index_events_on_location_id"
     t.index ["open_to"], name: "index_events_on_open_to"
     t.index ["recurrence_type"], name: "index_events_on_recurrence_type"
+    t.index ["slug"], name: "index_events_on_slug", unique: true
     t.index ["start_time"], name: "index_events_on_start_time"
     t.index ["status", "start_time"], name: "index_events_on_status_and_start_time"
     t.index ["status", "visibility"], name: "index_events_on_status_and_visibility"
