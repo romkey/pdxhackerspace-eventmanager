72a7689e19cabfe49dadd2fe2b586d86e54402c7
John Romkey <58883+romkey@users.noreply.github.com>
2025-12-31 09:18:14 -0800

    Fix rubocop warnings in sidekiq support file

--- spec/support/sidekiq.rb
+++ spec/support/sidekiq.rb
@@ -1,14 +1,13 @@
 require 'sidekiq/testing'
 
 RSpec.configure do |config|
-  config.before(:each) do
+  config.before do
     # Use fake mode by default - jobs are pushed to a fake queue
     Sidekiq::Testing.fake!
   end
 
-  config.after(:each) do
+  config.after do
     # Clear any queued jobs after each test
     Sidekiq::Worker.clear_all
   end
 end
-
